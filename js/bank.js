354cc4a61c5a14d68076844e6faec2c1e59414c5
juhayerayaz <aiazj97@gmail.com>
2022-02-13 22:01:26 +0600

    first commit

---
+++ js/bank.js
@@ -0,0 +1,13 @@
+document.getElementById('login-btn').addEventListener
+('click', function(){
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+
+    if (userEmail == 'admin' && userPassword == 'admin'){
+        window.location.href = "banking.html";
+    }
+});
+
+
